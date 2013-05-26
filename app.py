3080fd47c9007ce4b4f9a7e508a557cc4171d15c
Karsten <jeskar@web.de>
2013-05-26 20:52:17 +0200

    Remnoved dependency on pylast

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+import json
+import requests
 from spotify_api.api import SpotifyApi
-import pylast
 
 app = Flask(__name__)
 
@@ -9,8 +11,25 @@ s_api = SpotifyApi()
 LASTFM_API_KEY = "b9ba81a4f80bbff4fa3fcf7df609947e"
 LASTFM_API_SECRET = "65f9cf1308e52e3369c5eeb992fa846a"
 
-network = pylast.LastFMNetwork(api_key = LASTFM_API_KEY, 
-                               api_secret = LASTFM_API_SECRET)
+def getRecentTracks(user):
+
+    url = "http://ws.audioscrobbler.com/2.0/"
+
+    payload = {"api_key": LASTFM_API_KEY, 
+              "method": "user.getrecenttracks",
+              "user": user,
+              "format": "json"}
+
+    response = requests.get(url, params = payload)
+    app.logger.debug(response.url)
+
+    if response.status_code != 200:
+        raise Exception("Panic! Cannot call lastfm user.getrecentracks!")
+
+    #process response
+    data = json.loads(response.text)
+
+    return data['recenttracks']['track']
 
 def isArtistOfAlbum(spotify_album, artist_name):
     for a in spotify_album.artists:
@@ -37,32 +56,31 @@ def getSpotifyLink(album_name, artist_name):
     print "Unknown for: %s" % (album_name)
     return "Unknown"
 
-def formatSpotifyLink(link):
-    return '<iframe src="https://embed.spotify.com/?uri=%s" \
-                    width="300" height="380" \
-                    frameborder="0" \
-                    allowtransparency="true"></iframe>' % (link)
-
-@app.route("/")
-def index():
-
-    user = network.get_user("thesimius")
+@app.route("/<username>")
+def index(username):
 
     #get recent tracks of user
-    tracks = [played_track[0] for played_track in user.get_recent_tracks()]
-   
+    app.logger.debug("Get recent tracks from user... %d" % datetime.now().second)
+    tracks = getRecentTracks(username)
+       
     #get unique album names
     albums = dict()
     for track in tracks:
-        albums[track.get_album().get_name()] = track.get_artist().get_name()
+        try:
+            albums[track['album']['#text']] = track['artist']['#text']
+        except AttributeError:
+            pass
 
-    album_links = [getSpotifyLink(album_name, artist_name) 
-                   for album_name, artist_name 
-                   in albums.items()]
+    #Get Spotify links and collect data
+    app.logger.debug("Look for Spotify links... %d" % datetime.now().second)
+    album_data = [(album_name, getSpotifyLink(album_name, artist_name))
+                  for album_name, artist_name 
+                  in albums.items()]
 
-    awesome_list = " ".join(formatSpotifyLink(a) for a in album_links)
+    print album_data
 
-    return "<h1>Magic Lists</h1>" + awesome_list
+    app.logger.debug("Render template... %d" % datetime.now().second)
+    return render_template("index.html", albums = album_data)
 
 if __name__=="__main__":
     app.run(debug = True)
